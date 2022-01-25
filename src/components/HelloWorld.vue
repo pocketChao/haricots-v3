a180a4adf174b2e8367607717a7ad01e18bf7211
liuzhichao <liuzc6788@163.com>
2022-01-25 09:51:56 +0800

    项目eslint 部分文件配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import useHome from '@/composables/useHome'
 
 defineProps<{ msg: string }>()
 
+const num = useHome()
+console.log(num.value)
 const count = ref(0)
 </script>
 
@@ -16,12 +19,13 @@ const count = ref(0)
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
   </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+  <p>
+    See
+    <code>README.md</code> for more information.
+  </p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
@@ -33,7 +37,7 @@ const count = ref(0)
   </p>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 a {
   color: #42b983;
 }
@@ -43,10 +47,24 @@ label {
   font-weight: bold;
 }
 
+.container {
+  .sub {
+    font-size: 20;
+    color: red;
+  }
+}
+
 code {
   background-color: #eee;
   padding: 2px 4px;
   border-radius: 4px;
   color: #304455;
+}
+</style>
+
+<style lang="scss" module>
+.hello-box {
+  padding: 10px;
+  background: red;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+/*
+ * @Author: LiuZhichao
+ * @Date: 2022-01-23 10:04:21
+ * @LastEditors: LiuZhichao
+ * @LastEditTime: 2022-01-24 15:51:29
+ * @FilePath: /haricots/vite.config.ts
+ * @Description: 
+ * Copyright (c) 2022 by LiuZhichao, All Rights Reserved. 
+ */
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { resolve } from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: "./",
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  },
+  server: {
+    port: 8080,
+    open: true
+  }
 })
